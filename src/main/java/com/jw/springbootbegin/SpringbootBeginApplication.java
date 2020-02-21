d625cef140e5c49f63fc8c1a8c82544be24709de
Jiangwei <466944168@qq.com>
2020-02-21 22:40:58 +0800

    init

---
+++ src/main/java/com/jw/springbootbegin/SpringbootBeginApplication.java
@@ -0,0 +1,13 @@
+package com.jw.springbootbegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBeginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jw/springbootbegin/SpringbootBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.springbootbegin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBeginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
